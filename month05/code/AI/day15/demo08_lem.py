4ae73debaaee9f87cd38ba1e4366b3b1c7e30345
tarena <1239269939@qq.com>
2019-09-22 17:21:28 +0800

    month05_AI_day06

---
+++ month05/code/AI/day15/demo08_lem.py
@@ -0,0 +1,12 @@
+"""
+demo08_lem.py 词干提取
+"""
+import nltk.stem as ns
+words = ['table', 'probably', 'wolves', 'playing',
+         'is', 'dog', 'the', 'beaches', 'grounded',
+         'dreamt', 'envision']
+lemmatizer = ns.WordNetLemmatizer()
+for word in words:
+    n_lemma = lemmatizer.lemmatize(word, pos='n')
+    v_lemma = lemmatizer.lemmatize(word, pos='v')
+    print('%8s %8s %8s' % (word, n_lemma, v_lemma))
